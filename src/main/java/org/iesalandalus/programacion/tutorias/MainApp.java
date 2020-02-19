e375383ca5e75e09d29cabfda7744223980b1f99
Mar <Mar@DESKTOP-7OECT65.home>
2020-02-19 11:15:45 +0100

    Modificación MainApp en la creación de modelo.

--- src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
@@ -1,9 +1,20 @@
 package org.iesalandalus.programacion.tutorias;
 
+import org.iesalandalus.programacion.tutorias.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.FactoriaFuenteDatos;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.IModelo;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.Modelo;
+import org.iesalandalus.programacion.tutorias.mvc.vista.Vista;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("Gestión de tutorías del IES Al-Ándalus");
+		
+		IModelo modelo = new Modelo(FactoriaFuenteDatos.MEMORIA.crear());
+		Vista vista = new Vista();
+		Controlador controlador = new Controlador(modelo, vista);
+		controlador.comenzar();
 	}
 
 }
